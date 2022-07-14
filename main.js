5b9c305611214866e9428d12eda67b8b882094a2
양혜정 <102224554+HYEJUNGYANG@users.noreply.github.com>
2022-07-15 02:23:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const yes = document.querySelector('#yes');
 const no = document.querySelector('#no');
+const p = document.querySelector('p');
+
 yes.onclick = () => {
   alert('맞아요!!! 인정하셨군요!! 🐸 구리 ㄱㅇㅇ');
 };
@@ -10,4 +12,20 @@ no.onmousemove = evt => {
   const dy = evt.offsetY;
   no.style.left = `${x - (dx - 40)}px`;
   no.style.top = `${y - (dy - 25)}px`;
+
+  const size = no.getBoundingClientRect();
+  if (
+    size.left < 0 ||
+    size.top < 0 ||
+    window.innerHeight < size.bottom ||
+    window.innerHeight - size.bottom < 20 ||
+    window.innerWidth < size.right
+  ) {
+    p.style.display = 'block';
+  }
+};
+p.onclick = () => {
+  p.style.display = 'none';
+  no.style.left = 0;
+  no.style.top = 0;
 };
